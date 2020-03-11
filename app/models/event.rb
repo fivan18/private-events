d9bd984b0bc733d4252959b3145b689b94fac7f1
certilremy <certilremy@gmail.com>
2020-03-11 17:30:50 -0400

    display event by past ans upcomming

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   default_scope { order('date DESC') }
+  scope :past, -> { where('date < ?', Time.now) }
+  scope :upcom, -> { where('date > ?', Time.now) }
   belongs_to :creator, class_name: 'User'
   has_many :attendences, foreign_key: 'attended_event', class_name: 'Attendence'
   has_many :attendees, through: :Attendences
