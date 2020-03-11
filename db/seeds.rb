8381065c840156769ec737a21505279d369da3df
certilremy <certilremy@gmail.com>
2020-03-11 17:17:43 -0400

    display attended event on user profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,16 @@ a.email = "certilremy@gmail.com"
 a.password = "123456"
 a.save
 
+c = User.new 
+c.username = "certilremy2"
+c.email = "certilremy2@gmail.com"
+c.password = "123456"
+c.save
+
 b = Event.new
 b.title = "festival music"
 b.location = "Haiti"
 b.date = Date.new 
 b.description = "blaaaa blaaaa"
-b.creator_id = a
+b.creator_id = a.id
 b.save
