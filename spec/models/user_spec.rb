7b16d6b07ceb62adafe5978b7a572b9053cd9a16
Ivan <fivanunam@gmail.com>
2020-03-12 11:18:40 -0600

    add test to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations to test' do
+    it do
+      should validate_presence_of(:username)
+    end
+    it do
+      should validate_presence_of(:email)
+    end
+  end
+  describe 'associations to test' do
+    it do
+      should have_many(:events)
+    end
+    it do
+      should have_many(:attended_events)
+    end
+  end
 end
