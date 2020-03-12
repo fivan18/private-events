cd169e008f4319a5351631107858c4a93a44a24a
Ivan <fivanunam@gmail.com>
2020-03-12 12:10:01 -0600

    refactor model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,19 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations to test' do
-    it do
-      should validate_presence_of(:username)
-    end
-    it do
-      should validate_presence_of(:email)
-    end
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
   end
   describe 'associations to test' do
-    it do
-      should have_many(:events)
-    end
-    it do
-      should have_many(:attended_events)
-    end
+    it { should have_many(:events) }
+    it { should have_many(:attended_events) }
   end
 end
